255ef1c1a240fd1efa837463f266fe6a0b02280a
平凡公子 <282729573@qq.com>
2018-09-01 23:31:13 +0800

    fix: add src，开发事件订阅发布功能，新建router文件

---
+++ src/lib/router.js
@@ -0,0 +1,18 @@
+/**
+ * 路由模块
+ * 继承事件管理系统
+ */
+import EventManage from './eventManage';
+
+const eventManage = new EventManage;
+
+class Router {
+	constructor() {
+
+		
+	}
+
+	add(path, callBack) {
+
+	}
+}
